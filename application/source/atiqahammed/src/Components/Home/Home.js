7677d20649643fd08751dcc4cc0f0ac199419d7c
Atiq Ahammed <atiqahammedshamim@gmail.com>
2020-08-25 00:58:20 +0600

    FIX: warning resolved

--- application/source/atiqahammed/src/Components/Home/Home.js
+++ application/source/atiqahammed/src/Components/Home/Home.js
@@ -1,20 +1,19 @@
 import React, { Fragment } from 'react';
-import grtteingImage from '../../stuffs/img/Hi.png';
 import About from './About';
 
 function Home(props) {
     return (
         <Fragment>
-            <div class="container">
-                <div class="row">
-                    <div class="col"></div>
-                    <div class="col-8 pt-100">
+            <div className="container">
+                <div className="row">
+                    <div className="col"></div>
+                    <div className="col-8 pt-100">
                         <div>
                             <About />
                         </div>
                         
                     </div>
-                    <div class="col"></div>
+                    <div className="col"></div>
                 </div>
             </div>
 
--- application/source/atiqahammed/src/Components/Navbar/NavLinks.js
+++ application/source/atiqahammed/src/Components/Navbar/NavLinks.js
@@ -1,5 +1,5 @@
 import React, { Fragment } from 'react';
-import { Link, NavLink } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 
 function NavLinks() {
     return (
